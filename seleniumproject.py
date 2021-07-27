ace58a58ee40525b699648e892536415aad1560c
grado93 <87993110+grado93@users.noreply.github.com>
2021-07-26 17:33:33 -0700

    Update seleniumproject.py

--- seleniumproject.py
+++ seleniumproject.py
@@ -1,9 +1,8 @@
-import selenium
 from selenium import webdriver
 from selenium.webdriver import safari 
-driver = safari
+driver = Safari()
 from selenium.webdriver.common.keys import keys
-driver.get("https://www.gooogle.com")
+driver.get("https://www.google.com")
 driver.find_element_by_name("q")
 driver.send_Keys("RTS Labs")
 driver.send_Keys(keys.Return)
